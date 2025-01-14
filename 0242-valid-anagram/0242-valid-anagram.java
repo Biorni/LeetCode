009d4814718a84cf93e120390a67842f25747d3b
Biorni <75360490+Biorni@users.noreply.github.com>
2025-01-14 21:58:24 +0100

    Time: 4 ms (76.33%), Space: 45.2 MB (7.13%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] charactersS = s.toCharArray();
+        char[] charactersT = t.toCharArray();
+
+        Arrays.sort(charactersS);
+        Arrays.sort(charactersT);
+
+        if(charactersS.length!=charactersT.length)
+            return false;
+
+        for(int i=0; i<charactersS.length; i++){
+            if(charactersS[i] != charactersT[i])
+                return false;
+        }
+
+        return true;
+    }
+}
