dadd8d854624dec8bc22c88de16158ad6ad37dd9
Biorni <75360490+Biorni@users.noreply.github.com>
2025-01-14 22:32:44 +0100

    Time: 34 ms (6.04%), Space: 43.2 MB (78.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -11,11 +11,11 @@ class Solution {
                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
             else
                 map.put(s.charAt(i),1);   
-        }
 
-        for(int i=0; i<t.length(); i++){
             if(map.containsKey(t.charAt(i)))
                 map.put(t.charAt(i),map.get(t.charAt(i))-1);
+            else
+                map.put(t.charAt(i),-1);       
         }
 
         System.out.println(map);
