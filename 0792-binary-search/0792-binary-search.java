3db424a07f07e3080b0a74f037d8d2e1a2f59be3
Biorni <75360490+Biorni@users.noreply.github.com>
2025-01-19 16:29:19 +0100

    Time: 0 ms (100%), Space: 45.8 MB (57.09%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,22 +3,19 @@ class Solution {
 
         int top = nums.length-1;
         int bot = 0;
-        // int mid = nums.length/2; /2
-
-        int jungleDiff = -1;
 
         while(bot<=top){
             int mid = (bot+top)/2;
             if(nums[mid]==target){
                 return mid;
             } else if(nums[mid]<target){
-                bot = mid + 1; //3
+                bot = mid + 1;
             } else {
                 top = mid -1;
             }
         }
 
-        return jungleDiff;
+        return -1;
         
     }
 }
