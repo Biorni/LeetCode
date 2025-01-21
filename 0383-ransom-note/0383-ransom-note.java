7519c893611992c1b1fe578bcbd2d72d118c4f7d
Biorni <75360490+Biorni@users.noreply.github.com>
2025-01-21 21:32:37 +0100

    Time: 18 ms (12.93%), Space: 45.3 MB (28.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,20 +3,20 @@ class Solution {
 
         HashMap<Character,Integer> note = new HashMap<>();
 
-        for(int i=0; i<ransomNote.length(); i++){
-            if(!note.containsKey(ransomNote.charAt(i)))
-                note.put(ransomNote.charAt(i),1);
+        for(char c: ransomNote.toCharArray()){
+            if(!note.containsKey(c))
+                note.put(c,1);
             else
-                note.put(ransomNote.charAt(i), note.get(ransomNote.charAt(i)) + 1);
+                note.put(c, note.get(c) + 1);
         }
 
-        for(int i=0; i<magazine.length(); i++){
-            if(note.containsKey(magazine.charAt(i)))
-                note.put(magazine.charAt(i), note.get(magazine.charAt(i)) - 1);
+        for(char c: magazine.toCharArray()){
+            if(note.containsKey(c))
+                note.put(c, note.get(c) - 1);
         }
 
-        for (char c : note.keySet()) {
-            if (note.get(c) > 0) {
+        for (int value : note.values()) {
+            if (value > 0) {
                 return false;
             }
         }
