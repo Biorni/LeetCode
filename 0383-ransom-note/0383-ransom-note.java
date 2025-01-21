3954e812cf72f404b3e43dff0c4321d935ec66f1
Biorni <75360490+Biorni@users.noreply.github.com>
2025-01-21 21:27:32 +0100

    Time: 23 ms (5.21%), Space: 45.2 MB (50.4%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        HashMap<Character,Integer> magazineMap = new HashMap<>();
+        HashMap<Character,Integer> note = new HashMap<>();
+
+        // for(int i=0; int i<magazine.length; int i++){
+        //     if(!magazineMap.get(magazine.toCharAt(i)))
+        //         magazineMap.put(magazine.toCharAt(i),1);
+        //     else
+        //         map.put(magazine.toCharAt(i), map.get(magazine.toCharAt(i)) + 1);
+        // }
+
+        for(int i=0; i<ransomNote.length(); i++){
+            if(!note.containsKey(ransomNote.charAt(i)))
+                note.put(ransomNote.charAt(i),1);
+            else
+                note.put(ransomNote.charAt(i), note.get(ransomNote.charAt(i)) + 1);
+        }
+
+        for(int i=0; i<magazine.length(); i++){
+            if(note.containsKey(magazine.charAt(i)))
+                note.put(magazine.charAt(i), note.get(magazine.charAt(i)) - 1);
+        }
+
+        boolean mapIsEmpty = true;
+        for (int value : note.values()) {
+            if (value > 0) {
+                mapIsEmpty = false;
+                break;
+            }
+        }
+
+        System.out.println(note);
+
+        return mapIsEmpty;
+    }
+}
